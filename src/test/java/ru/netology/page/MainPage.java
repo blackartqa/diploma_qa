95b1c281a2790ca6c040e5d42bec518d29207e3b
Artem Chernyshov <black_art@list.ru>
2022-02-08 22:24:25 +0300

    selectors repair

--- src/test/java/ru/netology/page/MainPage.java
+++ src/test/java/ru/netology/page/MainPage.java
@@ -2,12 +2,12 @@ package ru.netology.page;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class MainPage {
-    private SelenideElement buttonBuy = $("[class='button button_size_m button_theme_alfa-on-white']");
-    private SelenideElement buttonBuyByCredit = $$("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").get(0);
+    private SelenideElement buttonBuy = $$(".button").find(exactText("Купить"));
+    private SelenideElement buttonBuyByCredit = $$(".button").find(exactText("Купить в кредит"));
 
 
     public PaymentPage buyWithoutCredit() {
--- src/test/java/ru/netology/page/PaymentPage.java
+++ src/test/java/ru/netology/page/PaymentPage.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.SelenideElement;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -13,9 +14,9 @@ public class PaymentPage {
     private SelenideElement fieldCardNumber = $("[placeholder='0000 0000 0000 0000']");
     private SelenideElement fieldMonth = $("[placeholder='08']");
     private SelenideElement fieldYear = $("[placeholder='22']");
-    private SelenideElement fieldOwner = $$("[class='input__control']").get(3);
+    private SelenideElement fieldOwner = $$(".input").find(exactText("Владелец")).$(".input__control");
     private SelenideElement fieldCvc = $("[placeholder='999']");
-    private SelenideElement buttonContinue = $$("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").get(1);
+    private SelenideElement buttonContinue = $$(".button").find(exactText("Продолжить"));
 
     private SelenideElement bankApproved = $(withText("Операция одобрена Банком."));
     private SelenideElement bankRefusal = $(withText("Ошибка! Банк отказал в проведении операции."));
